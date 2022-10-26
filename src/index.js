7aba2af402cbad9fd30fe4154dbce031196b0066
FunWithAlbi <68329886+FunWithAlbiYT@users.noreply.github.com>
2022-10-26 11:21:06 -0500

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import { dom } from './New.js';
+
+var cunt = 0;
+
+function count() {
+    return `
+        <p>Counter: ${cunt}</p>
+    `;
+}
+
+var main = dom.create(document.getElementById("root"));
+
+document.getElementById("inc").addEventListener('event', function() {
+	cunt++
+	main.render(count);
+});
+
+main.render(count);
